58fe817251f477b52da1a26d6da632cd11dc56b3
Udesh-Ranjan <ranjanudesh404@gmail.com>
2020-03-22 12:12:09 +0530

    ArcWorld is an exploration in the endless space.

---
+++ src/arcworld/ArcWorld.java
@@ -0,0 +1,59 @@
+package arcworld;
+
+import arcworld.level1.Level1;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Dev Parzival
+ */
+public class ArcWorld extends JFrame implements Runnable{
+
+    Dimension size;
+    Level1 level1;
+    
+    public ArcWorld(){
+        size=new Dimension(1000,700);
+        this.setLayout(null);
+        this.setSize(size);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+        
+    }
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(0,0,1000,700);
+    }
+    @Override
+    public void run() {
+        //////////Level1/////////
+        setBackground(Color.black);
+        setForeground(Color.cyan);
+        level1=new Level1(900,700,this,getForeground(),getBackground());
+        addKeyListener(level1);
+        addWindowListener(level1);
+        level1.setBounds(0, 0, 700, 700);
+        add(level1);
+        Thread t=new Thread(level1);
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ArcWorld.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Exiting The thread");
+        ////////////Level2///////////
+    }
+    
+    public static void main(String[] args) {
+        ArcWorld arcadeWorld=new ArcWorld();
+        Thread game=new Thread(arcadeWorld);
+        game.start();
+    }
+}
---
+++ src/arcworld/level1/Bullet.java
@@ -0,0 +1,56 @@
+package arcworld.level1;
+
+
+import arcworld.level1.Level1;
+import java.awt.Graphics2D;
+
+public class Bullet{
+        int startx;
+        int starty;
+        int length;                                             //Length of the Bullet.
+        long start_time;
+        long curr_time;
+        static final long elapsedLimit=5;                      //Specifies when to update the bullet In Milliseconds.
+        static final int sep=30;                               //Minimum Seperation between the bullets on the same x axis.
+        Graphics2D g;                                          //Represents the Graphics object of a BufferedImage
+        Level1 panel;
+        Bullet(int x,int y,int len,Graphics2D _g,Level1 mi,long stime){
+//            synchronized(_g){
+                startx=x;
+                starty=y;
+                length=len;
+                g=_g;
+                panel=mi;
+                start_time=stime;
+//            }
+        }
+        void drawBullet(){
+//            synchronized(g){
+                g.setColor(panel.front);
+                g.drawLine(startx,starty,startx,starty+length);
+//                panel.repaint();
+//            }
+        }
+        void clearBullet(){
+//            synchronized(g){
+                g.setColor(panel.back);
+                g.drawLine(startx,starty,startx,starty+length);
+                g.setColor(panel.front);
+//                panel.repaint();
+//            }
+        }
+        public boolean equals(Bullet bullet){
+            if(bullet==null)return false;
+            return bullet.startx==startx&&bullet.starty==starty;
+        }
+        /////////////For Spacing Between The Bullets////////////////
+        public boolean equalsConsideringSep(Bullet bullet){
+            if(bullet==null)return false;
+            if(bullet.startx==startx)
+            if(Math.abs(bullet.starty-starty)<sep){
+                return true;
+            }
+            return false;
+        }
+}
+    
