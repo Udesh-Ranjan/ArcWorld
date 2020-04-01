e565e1c108580c9bd409b6bded3900a5d88c98f1
Udesh-Ranjan <ranjanudesh404@gmail.com>
2020-04-01 22:31:50 +0530

    Bullet

---
+++ src/arcworld/level3/Bullet.java
@@ -0,0 +1,57 @@
+
+package arcworld.level3;
+
+/**
+ *
+ * @author Dev Parzival
+ * @date   01-Apr-2020
+ * @time   20:22:59
+ */
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Bullet{
+    
+    static final int ySep=20;                                                    //Seperation b/w two bullets on same axis in pixels.
+    static final int xSep=5;
+    static final int step=3;                                                    //Speed at which the bullet will move.
+    
+    int startx;
+    int starty;
+    int length;                         
+    JPanel panel;
+    Bullet(int x,int y,int len,JPanel panel){
+            startx=x;
+            starty=y;
+            length=len;
+            this.panel=panel;
+    }
+    void drawBullet(Graphics g){
+            g.drawLine(startx,starty,startx,starty+length);
+    }
+
+    public void move(){
+        starty-=step;
+    }
+    public boolean bulletOutOfBound(){
+        return starty+length<=0?true:false;
+    }
+    public boolean equals(Bullet bullet){
+        if(bullet==null)return false;
+        return bullet.startx==startx&&bullet.starty==starty;
+    }
+    /**
+     * Returns boolean value based on the ySeparation b/w the bullets.
+     * @param bullet Bullet 
+     * @return boolean
+     */
+    public boolean equalsConsideringSep(Bullet bullet){
+        if(bullet==null)return false;
+        if(bullet.startx-startx<=xSep)
+        if(Math.abs(bullet.starty-starty)<ySep){
+            return true;
+        }
+        return false;
+    }
+}
+    
